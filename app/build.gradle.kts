b740eac5fb5a4133562a0e5617c0ff92bdf6465a
Williams Guerrero <williamsguerrero71@gmail.com>
2026-05-30 19:55:25 -0500

    chore: bump version to v1.2.0 (versionCode 4) for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.aistudio.controldegastos.pvwyzk"
     minSdk = 24
     targetSdk = 36
-    versionCode = 3
-    versionName = "1.1.2"
+    versionCode = 4
+    versionName = "1.2.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
